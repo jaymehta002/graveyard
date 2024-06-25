918c54fe088de3d8280c29dbf7aee72f2af90a53
jaymehta002 <jaymehta002@gmail.com>
2024-06-25 11:09:15 +0530

    products

--- src/app/products/all/page.tsx
+++ src/app/products/all/page.tsx
@@ -123,7 +123,7 @@ const Page = () => {
           )}
         </AnimatePresence>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
   {filteredProducts.map((product) => (
     <motion.div
       key={product.pid}

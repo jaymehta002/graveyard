b8aafda4bfa35d3827ec9d73c6a21bd7e8ccf0a4
jaymehta002 <jaymehta002@gmail.com>
2024-06-25 18:59:45 +0530

    2 cols

--- src/components/LandingPage/Trending.tsx
+++ src/components/LandingPage/Trending.tsx
@@ -38,7 +38,7 @@ const TrendingSection: React.FC = () => {
           </Link>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 sm:gap-6">
           {newDrops.map((card) => (
             <article
               key={card.pid}
